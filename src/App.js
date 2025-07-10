a3dc3ce2602fd1f5ee13a06602a2bde43d1f7788
oscar.chena <oscar.chena@skytel.com.py>
2025-07-10 14:12:16 -0300

    ups

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Aprende Jenkins en...
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 2
       </p>
     </div>
   );
